19bcfb7d507674e434a3f039a22730aefc95e5a9
Yuting Sun <sun.yut@husky.neu.edu>
2020-12-08 20:35:19 -0800

    result header

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import os
 from flask import Flask, render_template, request
-from beauty_search import index_sephora, search_query, get_product
+from beauty_search import load_data, index_sephora, search_query, get_product
 
 app = Flask(__name__)
 
 APP__ROOT = os.path.dirname(os.path.abspath(__file__))
-stopwords = set()
-inv_index = {}
-norm_weights = {}
-init_text = []
 
-
-
+df = load_data("./data/sephora_website_dataset.csv")
+print("data loaded")
 
 @app.route('/')
 def index():
@@ -19,7 +15,7 @@ def index():
 
 @app.route("/init", methods=['POST'])
 def initialize():
-    res = index_sephora()
+    res = index_sephora(df)
     init_text = [res]
 
     return render_template("index.html", init=init_text)
@@ -28,14 +24,13 @@ def initialize():
 def process_query():
     query = request.form['query']
     if not query:
-        query = "n/a"
         result = []
     else:
         k = 10
         ids, scores = search_query(query, k, "details")
 
         result = ['{}\t {:.4f} \t {}'.format(ids[i], scores[i], get_product(ids[i])) for i in range(k)]
-    return render_template("result.html", init=init_text, query=query, result=result)
+    return render_template("result.html", query=query, result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
