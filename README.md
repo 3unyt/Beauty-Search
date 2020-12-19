a0441a99b49b97e1235b006d09d6f6da84602a3a
Yuting Sun <sun.yut@husky.neu.edu>
2020-12-18 16:09:20 -0800

    add diversity to similar products

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 CS6200 Information Retrieval Final Project
 
 ## Run this project
-* Run Elasticsearch in elasticsearch directory: `bin/elasticsearch`
-* Run flass app: `python main.py`
+* Run Elasticsearch in elasticsearch directory: `bin/elasticsearch`.
+
+* If you run this app for the first time, run `python beauty_search.py` to index. The indexing process takes approximately one minute. Once the data is indexed, it does not need to be re-indexed.
+
+* Run flask app: `python main.py`.
--- beauty_search.py
+++ beauty_search.py
@@ -52,7 +52,8 @@ def basic_search(es, query, size):
         "query" : {
             "multi_match": { 
                 "query" : query,
-                "fields" : [ "brand^2", "name^2", "category", "details" ] 
+                "type" : "cross_fields",
+                "fields" : [ "brand^2", "name^2", "category^2", "details", "how_to_use"],
             }    
         },
 
@@ -77,16 +78,33 @@ def basic_search(es, query, size):
         
     return res['hits']['hits']
 
-def ingredients_search(es, query, size):
+def ingredients_search(es, query_ingredient, query_brand, size, exlude_brand=False):
+    if exlude_brand:
+        query={ 
+            "bool": { 
+                "must":{ 
+                    "match": { "ingredients": query_ingredient }
+                },
+                "must_not": {
+                    "match":  { "brand": query_brand }
+                }
+            }
+        }
+    else:
+        query = {
+            "match": {
+                "ingredients": {
+                    "query": query_ingredient,
+                    "analyzer": "stop",
+                    "zero_terms_query": "all",
+                    "minimum_should_match": "50%"
+                },
+            },
+        }
     body = {
         "from":0,
         "size": size,
-        "query" : {
-            "match": { 
-                "ingredients" : query,
-            }    
-        },
-
+        "query": query,
         "highlight" : {
             "pre_tags" : ["<mark>"],
             "post_tags" : ["</mark>"],
@@ -105,18 +123,39 @@ def get_product_result(es, query, size):
     for hit in hits:
         pid = hit['_id']
         result[pid] = hit['_source']
-        result[pid]['highlight'] = hit['highlight']['details'][0]
+        description = hit['_source']['details'].split(".")[0]
+        description = description.split(":")[1]
+        description = "What it is: "+description+"."
+        result[pid]["description"] = description
+        
     return result
 
 def get_similar_product(es, id):
-    ingredients = es.get(index="sephora", id=id)['_source']["ingredients"]
-    hits = ingredients_search(es, ingredients, 5)
+    query_source = es.get(index="sephora", id=id)['_source']
+    query_ingredients = query_source["ingredients"]
+    query_brand = query_source['brand']
+    hits = ingredients_search(es, query_ingredients, query_brand, 5)
     sim_result = {}
+    same_brand_count = 0
     for hit in hits:
-        pid = hit['_id']
-        if pid==id: continue
-        sim_result[pid] = hit['_source']
+        if same_brand_count > 2:
+            break
+        pid, source = hit['_id'], hit['_source']
+        if source["brand"] == query_source["brand"]:
+            same_brand_count += 1 
+        sim_result[pid] = source
         sim_result[pid]['highlight'] = hit['highlight']['ingredients'][0]
+    
+    if len(sim_result)==5:
+        return sim_result
+
+    ## get new hits that are from different brand
+    new_hits = ingredients_search(es, query_ingredients, query_brand, 2, exlude_brand=True)
+    for hit in new_hits:
+        pid, source = hit['_id'], hit['_source']
+        sim_result[pid] = source
+        sim_result[pid]['highlight'] = hit['highlight']['ingredients'][0]
+
     
     return sim_result
 
